fd5cb3678066522a54c136743546436d18e3b051
y66721986 <357491546@qq.com>
2018-11-22 04:23:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -67,9 +67,9 @@ iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 iptables -I INPUT -p tcp --dport 1723 -j ACCEPT
 #gre tunnel protocol
 iptables -I INPUT --protocol 47 -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 192.168.200.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
 #supposedly makes the vpn work better
-iptables -I FORWARD -s 10.10.10.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -I FORWARD -s 192.168.200.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 service pptpd restart
 END
 sh /etc/rc.local
